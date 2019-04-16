1bbbd5b77c482c04cb66d31ab9b29c9778291509
Chris Piccaro <chrispiccaro18@gmail.com>
2019-04-15 17:45:33 -0700

    we did it all

--- test/profiles.test.js
+++ test/profiles.test.js
@@ -50,7 +50,7 @@ describe('profile routes', () => {
 
   });
 
-  it.only('patches an existing profile', () => {
+  it('patches an existing profile', () => {
     return request(app)
       .post('/profile')
       .send({ name: 'name', favoriteCharacter: 'Bender' })
@@ -65,6 +65,21 @@ describe('profile routes', () => {
           favoriteCharacter: 'Fry',
           _id: expect.any(String),
           tagline: 'supp'
+        });
+      });
+  });
+
+  it('delete by id', () => {
+    return request(app)
+      .post('/profile')
+      .send({ name: 'name', favoriteCharacter: 'Bender' })
+      .then(createdProfile => {
+        return request(app)
+          .delete(`/profile/${createdProfile.body._id}`)
+      })
+      .then(result => {
+        expect(result.body).toEqual({
+          deleted: 1
         });
       });
   });
